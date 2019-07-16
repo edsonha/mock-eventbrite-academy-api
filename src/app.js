721a1800b4ff47e902086549e351648b636314e4
Dennis Lee <dennis-lcl@users.noreply.github.com>
2019-07-16 18:24:13 +0800

    [#9][Bran&Den] Add cors and whitelited dev/prod URLs.

--- src/app.js
+++ src/app.js
@@ -8,6 +8,28 @@ const UserModel = mongoose.model("user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const upcomingEventsRouter = require("./routes/upcoming-events.route");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: function(origin, callback) {
+      // allow requests with no origin
+      const allowedOrigins = [
+        "http://localhost:3000",
+        "https://stashaway-ui.herokuapp.com/"
+      ];
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  })
+);
 
 app.use(morgan("combined"));
 app.use(express.json());
