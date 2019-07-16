bba8aae9f309c52ea6399aea97d49953e88d9b8e
brandonngiam <brandonngiam@user.noreply.github.com>
2019-07-16 22:55:36 +0800

    Modified app.use(cors)

--- src/app.js
+++ src/app.js
@@ -21,12 +21,8 @@ app.use(
       // (like mobile apps or curl requests)
       if (!origin) return callback(null, true);
       if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
+        return callback(new Error("Not allowed by CORS"), false);
+      } else return callback(null, true);
     }
   })
 );
