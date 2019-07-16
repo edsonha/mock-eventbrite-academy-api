fc02659b33b1bd1d1348934e1cd7626e3b9be718
linjj93 <linjj93@hotmail.com>
2019-07-16 10:55:00 +0800

    [#10][JJ&Eddie] Add tests for login

--- __tests__/routes/route.test.js
+++ __tests__/routes/route.test.js
@@ -2,6 +2,7 @@ const { MongoClient } = require("mongodb");
 const request = require("supertest");
 const app = require("../../app");
 const mongoose = require("mongoose");
+const userData = require("../../data/user.data");
 require("../../db");
 
 describe("app", () => {
@@ -25,10 +26,50 @@ describe("app", () => {
 
   beforeEach(async () => {
     await db.dropDatabase();
+    const usersCollection = await db.collection("users");
+    await usersCollection.insertMany(userData);
   });
 
   it("should create a hello world app", async () => {
     const response = await request(app).get("/");
     expect(response.body).toBe("Hello World");
+  });
+
+  describe("login", () => {
+    it("should be able to login if correct email and password is given ", async () => {
+      const response = await request(app)
+        .post("/login")
+        .set("Content-Type", "application/json")
+        .send({ email: "john@gmail.com", password: "abcdefgh" });
+
+      expect(response.status).toBe(200);
+      expect(response.body.email).toBe("john@gmail.com");
+    });
+
+    it("should not be able to login if email cannot be found", async () => {
+      const response = await request(app)
+        .post("/login")
+        .set("Content-Type", "application/json")
+        .send({
+          email: "peter@gmail.com",
+          password: "password"
+        });
+
+      expect(response.status).toBe(401);
+      expect(response.body).toBe("User does not exist");
+    });
+
+    it("should not be able to login if wrong password is given", async () => {
+      const response = await request(app)
+        .post("/login")
+        .set("Content-Type", "application/json")
+        .send({
+          email: "john@gmail.com",
+          password: "secret"
+        });
+
+      expect(response.status).toBe(401);
+      expect(response.body).toBe("Wrong credentials");
+    });
   });
 });
