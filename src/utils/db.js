c91bb3da20ad400002a98e85b07b48976aa02593
brandonngiam <brandonngiam@user.noreply.github.com>
2019-07-17 15:20:22 +0800

    Removed DBURI from console.log

--- src/utils/db.js
+++ src/utils/db.js
@@ -13,5 +13,5 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 
 db.once("open", () => {
-  console.log("MongoDB connected", dbURI);
+  console.log("MongoDB connected");
 });
