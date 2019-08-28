8aa7455a2370749ef4af55bb31039dee71684d1b
edsonha <edsonha@gmail.com>
2019-08-28 16:17:07 +0800

    Deploy to CircleCI and Heroku

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const app = require("./app");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Listening to the smooth sound on PORT ${PORT}`);
